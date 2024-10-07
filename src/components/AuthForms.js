95b1bee66c4846d1a7a91b85e422147ec002c5d5
Rose Parry <rose729parry@gmail.com>
2024-10-07 10:59:36 +0200

    Change a default style

--- src/components/AuthForms.js
+++ src/components/AuthForms.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Card = styled.div`
   box-sizing: border-box;
@@ -26,10 +26,10 @@ const Input = styled.input`
 const Button = styled.button`
   background: linear-gradient(to bottom, #6371c7, #5563c1);
   border-color: #3f4eae;
-  border-radius: 3px;
+  border-radius: 5px;
   padding: 1rem;
   color: white;
-  font-weight: 700;
+  font-weight: 800;
   width: 100%;
   margin-bottom: 1rem;
   font-size: 0.8rem;
