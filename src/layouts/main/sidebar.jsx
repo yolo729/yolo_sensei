5054769862f7bdd4dacc89e51665e1a9c5c3242b
Rose Parry <rose729parry@gmail.com>
2024-10-07 03:34:04 +0200

    Fix responsive issues

--- src/layouts/main/sidebar.jsx
+++ src/layouts/main/sidebar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="w-[280px] shrink-0 bg-[#1C2536] h-[100vh] relative">
+    <div className="w-[285px] shrink-0 bg-[#1C2536] h-[100vh] relative">
       <Link to={"/"}>
         <div className="w-[100%] h-[100px] dropped1 flex items-center px-[30px] drop-shadow-xl  relative">
           <img src="/bgLogo.svg" alt="" />
