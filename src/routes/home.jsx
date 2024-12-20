b47fb32843ea4676aa93345dd7643414c03ff5aa
Faith-Kimongo <faithvictor78@gmail.com>
2024-12-20 12:47:14 +0200

    import suspense

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 
 const HomePage = React.lazy(() => import("../views/index.jsx"));
 
